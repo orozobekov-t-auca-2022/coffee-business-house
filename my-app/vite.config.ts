a6267508919ad85917fb5c93ce90071b98ece2de
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2025-10-24 14:30:22 +0600

    fix: changing some parameters

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/orozobekov-t-auca-2022-JSFEQS2025Q3/',
+  base: '/coffee-house/',
   
   build: {
     outDir: 'dist',
