bf8ee80e34d3e01f72d3c182dc231deec36952fc
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2025-10-24 14:57:12 +0600

    fix:reseting yaml file to try deploy again

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coffee-house/',
+  base: '/orozobekov-t-auca-2022-JSFEQS2025Q3/',
   
   build: {
     outDir: 'dist',
