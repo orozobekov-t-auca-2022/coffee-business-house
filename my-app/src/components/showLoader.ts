9ec59b67a3c7124f3073ecf07507f9e481f4a483
orozobekov-t-auca-2022 <orozobekov-t-auca-2022@users.noreply.github.com>
2025-10-26 22:26:14 +0000

    deploy: 83500a4b7716ad6a57cf6c74bfcf8bf6d14b8793

--- my-app/src/components/showLoader.ts
+++
@@ -1,8 +0,0 @@
-export function showLoader(show: boolean) {
-  const loader = document.getElementById("loader");
-  if (!show){
-    loader?.classList.add("hidden");
-  } else {
-    loader?.classList.remove("hidden");
-  }
-}
--- my-app/src/services/favoritesService.ts
+++
@@ -1,7 +0,0 @@
-import type { Products } from "../types/product";
-
-export async function favoritesService(): Promise<Products> {
-    const res = await fetch(`${import.meta.env.VITE_COFFEE_API_KEY}/products/favorites`).then(res => res.json());
-    console.log(res)
-    return {products:res.data};
-}
