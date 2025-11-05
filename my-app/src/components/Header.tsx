fd0a687fad63a7b5c3c2ed70e433b40a546eb2de
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2025-11-05 21:03:20 +0600

    feat: added theme switcher for dark and light themes

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../provider/ThemeContext";
 
 function Header() {
     const [itemsInCart, setItemsInCart] = useState<number>(0);
     useEffect(() => {
         setItemsInCart(Number(JSON.parse(localStorage.getItem('cartItems') || '[]').length));
     }, [])
-    
+
+    const {theme, toggleTheme} = useContext(ThemeContext);
+
     return <>
         <header>
         <nav>
@@ -34,13 +37,30 @@ function Header() {
                         <li><Link to="#contact-us">Contact us</Link></li>
                     </ul>
                 </li>
+                <li className="themeChanger">
+                    <button onClick={() => {
+                        toggleTheme()
+                    }}>
+                        {
+                            (theme === 'dark') ? (
+                                <span className="material-symbols-outlined" style={{color: "rgba(225, 212, 201, 1)"}}>
+                                    dark_mode
+                                </span>
+                            ): (
+                                <span className="material-symbols-outlined">
+                                    light_mode
+                                </span>
+                            )
+                        }
+                    </button>
+                </li>
                 <ul className="cartAndMenu">
                     {
                         (localStorage.getItem('token')) ? (
                         <Link to={"/cart"} className="cartDisplay">
                             <svg className="cart-logo" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M16.0942 8.36255L17.1455 15.1959C17.3319 16.4074 16.3945 17.5 15.1688 17.5H4.83122C3.60545 17.5 2.66809 16.4074 2.85448 15.1959L3.90576 8.36255C4.05586 7.38689 4.89536 6.66667 5.88251 6.66667H14.1175C15.1046 6.66667 15.9441 7.38689 16.0942 8.36255Z" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                <path d="M11.6663 4.16667C11.6663 3.24619 10.9201 2.5 9.99967 2.5C9.0792 2.5 8.33301 3.24619 8.33301 4.16667" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                <path d="M16.0942 8.36255L17.1455 15.1959C17.3319 16.4074 16.3945 17.5 15.1688 17.5H4.83122C3.60545 17.5 2.66809 16.4074 2.85448 15.1959L3.90576 8.36255C4.05586 7.38689 4.89536 6.66667 5.88251 6.66667H14.1175C15.1046 6.66667 15.9441 7.38689 16.0942 8.36255Z" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                <path d="M11.6663 4.16667C11.6663 3.24619 10.9201 2.5 9.99967 2.5C9.0792 2.5 8.33301 3.24619 8.33301 4.16667" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'}  strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                             <span className="productsAmount">{itemsInCart}</span>
                         </Link>
@@ -49,10 +69,10 @@ function Header() {
                     <li className="nav-menu">
                         <Link to="/menu">Menu</Link>
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M14.167 9.76667V11.6667C14.167 14.8883 11.5553 17.5 8.33366 17.5C5.112 17.5 2.50033 14.8883 2.50033 11.6667V9.76667C2.50033 9.4353 2.76896 9.16667 3.10033 9.16667H13.567C13.8984 9.16667 14.167 9.4353 14.167 9.76667Z" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M10.0003 7.50008C10.0003 6.66675 10.5956 5.83341 11.786 5.83341C13.101 5.83341 14.167 4.76743 14.167 3.45246V2.91675" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M6.66634 7.5V7.08333C6.66634 5.70262 7.78563 4.58333 9.16634 4.58333C10.0868 4.58333 10.833 3.83714 10.833 2.91667V2.5" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M13.333 9.16675H15.4163C16.5669 9.16675 17.4997 10.0995 17.4997 11.2501C17.4997 12.4007 16.5669 13.3334 15.4163 13.3334H14.1663" stroke="#403F3D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M14.167 9.76667V11.6667C14.167 14.8883 11.5553 17.5 8.33366 17.5C5.112 17.5 2.50033 14.8883 2.50033 11.6667V9.76667C2.50033 9.4353 2.76896 9.16667 3.10033 9.16667H13.567C13.8984 9.16667 14.167 9.4353 14.167 9.76667Z" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M10.0003 7.50008C10.0003 6.66675 10.5956 5.83341 11.786 5.83341C13.101 5.83341 14.167 4.76743 14.167 3.45246V2.91675" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M6.66634 7.5V7.08333C6.66634 5.70262 7.78563 4.58333 9.16634 4.58333C10.0868 4.58333 10.833 3.83714 10.833 2.91667V2.5" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M13.333 9.16675H15.4163C16.5669 9.16675 17.4997 10.0995 17.4997 11.2501C17.4997 12.4007 16.5669 13.3334 15.4163 13.3334H14.1663" stroke={theme === 'dark' ? 'rgba(225, 212, 201, 1)' : '#403F3D'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </li>
                 </ul>
---
+++ my-app/src/provider/ThemeProvider.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(prev => {
+            const next = prev === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', next);
+            return next;
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return(
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider;
